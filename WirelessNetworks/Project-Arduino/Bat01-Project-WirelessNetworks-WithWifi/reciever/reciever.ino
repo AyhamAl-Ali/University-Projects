66e31498b484c691d0d474d1f85a9117e477059d
Ayham Al-Ali <alali_ayham@yahoo.com>
2023-01-12 08:22:19 +0300

    Upload Test 1

--- WirelessNetworks/Project-Arduino/Bat01-Project-WirelessNetworks-WithWifi/reciever/reciever.ino
+++ WirelessNetworks/Project-Arduino/Bat01-Project-WirelessNetworks-WithWifi/reciever/reciever.ino
@@ -1,50 +1,91 @@
-/* This simple project describes how to make an ultrasonic alarm system using 
-LED, Ultasonic Sensor(HC-SR04) and a buzzer.*/ 
+// /* This simple project describes how to make an ultrasonic alarm system using 
+// LED, Ultasonic Sensor(HC-SR04) and a buzzer.*/
 
-//Firstly the connections of ultrasonic Sensor.Connect +5v and GND normally and trigger pin to 12 & echo pin to 13. 
+// //Firstly the connections of ultrasonic Sensor.Connect +5v and GND normally and trigger pin to 12 & echo pin to 13.
 
-#define trigPin 12
-#define echoPin 13
-int Buzzer = 8; // Connect buzzer pin to 8
-int ledPin= 6;  //Connect LEd pin to 6
-int duration, distance; //to measure the distance and time taken 
+// // #define trigPin 12
+// // #define echoPin 13
+// // int Buzzer = 8;          // Connect buzzer pin to 8
+// // int ledPin = 6;          //Connect LEd pin to 6
+// // int duration, distance;  //to measure the distance and time taken
 
-int randomBuzzerSound[8] = {800};
-int randomBuzzerSoundArrayLength = 1;
+// // int randomBuzzerSound[8] = { 800 };
+// // int randomBuzzerSoundArrayLength = 1;
+
+// void setup() {
+//   // Serial.begin(9600);
+//   // //Define the output and input objects(devices)
+//   // pinMode(trigPin, OUTPUT);
+//   // pinMode(echoPin, INPUT);
+//   // pinMode(Buzzer, OUTPUT);
+//   // pinMode(ledPin, OUTPUT);
+// }
+
+// void loop() {
+
+//   // digitalWrite(trigPin, HIGH);
+//   // delayMicroseconds(10);
+//   // digitalWrite(trigPin, LOW);
+//   // duration = pulseIn(echoPin, HIGH);
+//   // distance = (duration / 2) / 29.1;
+//   // //when distance is greater than or equal to 200 OR less than or equal to 0,the buzzer and LED are off
+//   // if (distance >= 100 || distance <= 0)  // OFF
+//   // {
+//   //   Serial.println("No object detected");
+//   //   digitalWrite(Buzzer, LOW);
+//   //   digitalWrite(ledPin, LOW);
+//   //   noTone(Buzzer);
+//   // } else {
+//   //   // Serial.println("object detected");
+//   //   Serial.print("distance = ");
+//   //   Serial.print(distance);
+//   //   Serial.print("\n");
+//   //   // Serial.print(distance);        //prints the distance if it is between the range 0 to 200
+//   //   // tone(Buzzer, randomBuzzerSound[random(0,randomBuzzerSoundArrayLength-1)]);
+//   //   // tone(Buzzer, randomBuzzerSound[0]);
+//   //   digitalWrite(ledPin, HIGH);
+//   // }
+// }
+
+/*
+ * Connects to WiFi network
+ * Starts WiFi server with fixed IP
+ * Sends data to client
+ * 
+ * Made by: How To Everything
+ * https://www.youtube.com/channel/UCJVtkE-cJj3g-KKhZuSMe1w
+ */
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+
+char ssid[] = "********";  //Name of your WiFi network              
+char pass[] = "********";  //Password of your WiFi network               
+WiFiServer server(80);
+                    
+IPAddress ip(192, 168, 1, 4);  //Reserved Ip address for your server            
+IPAddress gateway(192,168,0,1);  //Gateway of your network          
+IPAddress subnet(255,255,255,0);  //Subnet mask of your network         
 
 void setup() {
-        Serial.begin (9600); 
-        //Define the output and input objects(devices)
-        pinMode(trigPin, OUTPUT); 
-        pinMode(echoPin, INPUT);
-        pinMode(Buzzer, OUTPUT);
-        pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);                   
+  WiFi.config(ip, gateway, subnet);  //Sets configuration for WiFi network       
+  WiFi.begin(ssid, pass);            //Connects to network     
+  while (WiFi.status() != WL_CONNECTED) {  //If not connected to network
+    Serial.println("Not Connected!");      //Print this
+    delay(500);
+  }
+  Serial.println("Connected!"); //When connected print this
+  server.begin();                   //Starts server      
 }
 
-void loop() {
-
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = (duration/2) / 29.1;
-    //when distance is greater than or equal to 200 OR less than or equal to 0,the buzzer and LED are off
-  if (distance >= 100 || distance <= 0) // OFF
-        {
-        Serial.println("No object detected");
-        digitalWrite(Buzzer,LOW);
-        digitalWrite(ledPin,LOW);
-        noTone(Buzzer);
-        }
-  else {
-        // Serial.println("object detected");
-        Serial.print("distance = ");              
-        Serial.print(distance);              
-        Serial.print("\n");
-        // Serial.print(distance);        //prints the distance if it is between the range 0 to 200
-        tone(Buzzer, randomBuzzerSound[random(0,randomBuzzerSoundArrayLength-1)]);
-        // tone(Buzzer, randomBuzzerSound[0]);
-        digitalWrite(ledPin,HIGH);
+void loop () {
+  WiFiClient client = server.available();
+  if (client) {
+    if (client.connected()) {
+      String request = client.readStringUntil('\r');    
+      client.flush();
+      client.println(Serial.read()); //Sends data recieved from Arduino
+    }
+    client.stop();   //Terminates the connection with the client             
   }
 }
-
--- WirelessNetworks/Project-Arduino/Bat01-Project-WirelessNetworks-WithWifi/server/server.ino
+++ WirelessNetworks/Project-Arduino/Bat01-Project-WirelessNetworks-WithWifi/server/server.ino
@@ -3,80 +3,141 @@ LED, Ultasonic Sensor(HC-SR04) and a buzzer.*/
 
 //Firstly the connections of ultrasonic Sensor.Connect +5v and GND normally and trigger pin to 12 & echo pin to 13. 
 
+#include <SoftwareSerial.h>
+#include <ESP8266IoTAdapter.h>
 
-#include <SoftwareSerial.h>                        
-SoftwareSerial esp8266(10,11);                   
-#define serialCommunicationSpeed 9600               
-#define DEBUG true                                 
+/*
+ * Replace the SSID and password by yourself.
+ */
+const String WIFI_SSID = "Ayham";
+const String WIFI_PASSWORD = "123456787";
 
-void setup()
+/*
+ * Wire the temperature sensor LM35 to analog input Pin1.
+ */
+// const int LM35_PIN = 1;
 
+bool wifiConnected = false;
+String ip;
+String url;
+
+// Create ESP8266 adapter with a SoftwareSeiral
+SoftwareSerial softSerial(2, 3);
+ESP8266IoTAdapter esp(softSerial);
+
+void setupWifiStationMode()
 {
-  Serial.begin(serialCommunicationSpeed);           
-  esp8266.begin(serialCommunicationSpeed);     
-  InitWifiModule();                              
+    Serial.print("Connecting to WiFi AP...");
+
+    /*
+     * You need to replace the SSID and password by yourself.
+     */
+    esp.connectToAP(WIFI_SSID, WIFI_PASSWORD);
+    while (true)
+    {
+        int status = esp.getWifiStatus();
+        if (status == 2)
+        {
+            Serial.println("ERROR:");
+            Serial.println("Wrong password.");
+            break;
+        }
+        else if (status == 3)
+        {
+            Serial.println("ERROR:");
+            Serial.println("SSID \"" + WIFI_SSID + "\" has not been found.");
+            break;
+        }
+        else if (status == 4)
+        {
+            Serial.println("ERROR:");
+            Serial.println("Fail to connect with \"" + WIFI_SSID + "\".");
+            break;
+        }
+        else if (status == 5)
+        {
+            wifiConnected = true;
+            Serial.println();
+            ip = esp.getIP();
+            url = "http://" + ip + "/data";
+            Serial.println("IP Address: " + ip);
+            break;
+        }
+        else
+        {
+            delay(1000);
+            Serial.print(".");
+        }
+    }
+
+    if (wifiConnected)
+    {
+        Serial.print("Restarting server...");
+        esp.restartServer();
+        Serial.println("done");
+
+        Serial.println();
+        Serial.println("Please open your browser and visit");
+        Serial.println(url);
+        Serial.println();
+    }
 }
 
-void loop()                                                         
-{
+#define trigPin 12
+#define echoPin 13
+int Buzzer = 8; // Connect buzzer pin to 8
+int ledPin= 6;  //Connect LEd pin to 6
+int duration, distance; //to measure the distance and time taken 
 
-  if(esp8266.available())                                           
- {    
-    if(esp8266.find("+IPD,"))
-    {
-     delay(1000);
- 
-     int connectionId = esp8266.read()-48;                                                
-     String webpage = "<h1>Hello World!</h1>";
-     String cipSend = "AT+CIPSEND=";
-     cipSend += connectionId;
-     cipSend += ",";
-     cipSend +=webpage.length();
-     cipSend +="\r\n";
-     
-     sendData(cipSend,1000,DEBUG);
-     sendData(webpage,1000,DEBUG);
- 
-     String closeCommand = "AT+CIPCLOSE="; 
-     closeCommand+=connectionId; // append connection id
-     closeCommand+="\r\n";    
-     sendData(closeCommand,3000,DEBUG);
-    }
+int randomBuzzerSound[8] = {800};
+int randomBuzzerSoundArrayLength = 1;
+
+void setup() {
+    Serial.begin (9600); 
+    //Define the output and input objects(devices)
+    pinMode(trigPin, OUTPUT); 
+    pinMode(echoPin, INPUT);
+    pinMode(Buzzer, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+
+    // Serial.begin(9600);
+    setupWifiStationMode();
+}
+
+void loop() {
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    //when distance is greater than or equal to 200 OR less than or equal to 0,the buzzer and LED are off
+  if (distance >= 100 || distance <= 0) // OFF
+        {
+        Serial.println("No object detected");
+        digitalWrite(Buzzer,LOW);
+        digitalWrite(ledPin,LOW);
+        noTone(Buzzer);
+        }
+  else {
+        // Serial.println("object detected");
+        Serial.print("distance = ");              
+        Serial.print(distance);              
+        Serial.print("\n");
+        // Serial.print(distance);        //prints the distance if it is between the range 0 to 200
+        // tone(Buzzer, randomBuzzerSound[random(0,randomBuzzerSoundArrayLength-1)]);
+        // tone(Buzzer, randomBuzzerSound[0]);
+        digitalWrite(ledPin,HIGH);
+  }
+
+  if (wifiConnected)
+  {
+      // Read temperature value from Analog PIN 0
+      // double temperature = analogRead(LM35_PIN) * (5/10.24);
+      // Update values on ESP8266 server every 60 seconds
+      // esp.setValue("temperature", temperature);
+      // Serial.print("Temperature: ");
+      // Serial.println(temperature);
+      // delay(10000);
   }
 }
-
-String sendData(String command, const int timeout, boolean debug)
-{
-    String response = "";                                             
-    esp8266.print(command);                                          
-    long int time = millis();                                      
-    while( (time+timeout) > millis())                                 
-    {      
-      while(esp8266.available())                                      
-      {
-        char c = esp8266.read();                                     
-        response+=c;                                                  
-      }  
-    }    
-    if(debug)                                                        
-    {
-      Serial.print(response);
-    }    
-    return response;                                                  
-}
-
-void InitWifiModule()
-{
-  sendData("AT+RST\r\n", 2000, DEBUG);                                                  
-  sendData("AT+CWJAP=\"WIFI_name\",\"pwd\"\r\n", 2000, DEBUG);        
-  delay (3000);
-  sendData("AT+CWMODE=1\r\n", 1500, DEBUG);                                             
-  delay (1500);
-  sendData("AT+CIFSR\r\n", 1500, DEBUG);                                             
-  delay (1500);
-  sendData("AT+CIPMUX=1\r\n", 1500, DEBUG);                                             
-  delay (1500);
-  sendData("AT+CIPSERVER=1,80\r\n", 1500, DEBUG);                                     
-
-}
-
